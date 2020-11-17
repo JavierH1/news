c509ae683b6f5895681e749a71e3399d28defe46
JavierH1 <javier.hidalgo@alumnos.ucn.cl>
2020-11-17 02:10:10 -0300

    feat(Project): add class News , ContractsImpl and interface Contracts

---
+++ app/src/main/java/cl/ucn/disc/dsm/jhidalgo/news/model/News.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2020 Javier Hidalgo Ochoa, javier.hidalgo@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.jhidalgo.news.model;
+
+
+import org.threeten.bp.ZonedDateTime;
+
+/**
+ * The domain model: News.
+ * @author Javier Hidalgo Ochoa.
+ */
+public class News {
+    /**
+     * Unique id
+     */
+    private Long id;
+
+    /**
+     *The Tittle.
+     * Restrictions: not null, size > 2;
+     */
+    private String tittle;
+
+    /**
+     *The source
+     */
+    private String source;
+
+    /**
+     *The author.
+     */
+    private String author;
+
+    /**
+     *The URL.
+     */
+    private String url;
+
+    /**
+     *The URL of the image.
+     */
+    private String urlImage;
+
+    /**
+     *The description.
+     */
+    private String description;
+
+    /**
+     *The content.
+     */
+    private String content;
+
+    /**
+     *The date of publish.
+     */
+    private ZonedDateTime publishedAt;
+
+    /**
+     * The constructor.
+     * @param id
+     * @param tittle
+     * @param source
+     * @param author
+     * @param url
+     * @param urlImage
+     * @param description
+     * @param content
+     * @param publishedAt
+     */
+
+    public News(Long id, String tittle, String source, String author, String url, String urlImage, String description, String content, ZonedDateTime publishedAt) {
+        this.id = id;
+        this.tittle = tittle;
+        this.source = source;
+        this.author = author;
+        this.url = url;
+        this.urlImage = urlImage;
+        this.description = description;
+        this.content = content;
+        this.publishedAt = publishedAt;
+    }
+
+    /**
+     * @return the Id.
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @return the tittle.
+     */
+    public String getTittle() {
+        return tittle;
+    }
+
+    /**
+     * @return the source.
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * @return the author.
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @return the URL.
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @return the URL of the image.
+     */
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    /**
+     * @return the description.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @return the content.
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @return the date of publish.
+     */
+    public ZonedDateTime getPublishedAt() {
+        return publishedAt;
+    }
+}
---
+++ app/src/main/java/cl/ucn/disc/dsm/jhidalgo/news/services/ContractsImpl.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 Javier Hidalgo Ochoa, javier.hidalgo@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.jhidalgo.news.services;
+
+import com.github.javafaker.Faker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.threeten.bp.ZonedDateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.ucn.disc.dsm.jhidalgo.news.model.News;
+
+/**
+ * @author Javier Hidalgo Ochoa
+ */
+public class ContractsImpl implements Contracts {
+
+
+    /**
+     * The logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(ContractsImpl.class);
+    /**
+     * Get the list of News.
+     *
+     * @param size size of the list.
+     * @return the list of News.
+     */
+    @Override
+    public List<News> retrieveNews(Integer size) {
+
+        // T he list of news
+        final List<News> news = new ArrayList<>();
+
+        // TODO: add the faker news to the list
+
+
+        for(int i=0;i < 5;i++){
+            Faker faker = Faker.instance();
+
+            Long number =Long.MIN_VALUE;
+
+            ZonedDateTime time = ZonedDateTime.parse("2012-06-30T12:30:40Z[UTC]");
+
+            News news1 = new News(number+i, faker.lorem().sentence(), faker.app().name(), faker.name().fullName(),faker.internet().url(),faker.internet().url(),faker.lorem().sentence(),faker.lorem().paragraph(),time);
+            news.add(news1);
+
+        }
+
+
+
+        return news;
+    }
+}
