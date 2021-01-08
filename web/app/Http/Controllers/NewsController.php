e6af77bb1cebb43d2aed199cd9e032e033f68add
Bar011 <Bar011@alumnos.ucn.cl>
2021-01-08 04:08:52 -0300

    feat(NewsController):Add method of store

--- web/app/Http/Controllers/NewsController.php
+++ web/app/Http/Controllers/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\News;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class NewsController extends Controller
 {
@@ -14,7 +15,7 @@ class NewsController extends Controller
      */
     public function index()
     {
-        // SELECT * FROM News
+         //SELECT * FROM News
         $news = News::all();
 
         // Return the GET request with code 200
@@ -42,18 +43,60 @@ class NewsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // The validation of the required
+        $this->validate($request, [
+            'title' => 'required',
+            'author' => 'required',
+            'source' => 'required',
+            'url' => 'required',
+            'description' => 'required',
+            'content' => 'required',
+            'date' => 'required'
+        ]);
+
+        // Creates a News
+        $news = new News();
+
+        // Get  the title of the news
+        $news->title = $request->input('title');
+
+        // Get the author of the news
+        $news->author = $request->input('author');
+
+        // Get the source of the news
+        $news->source = $request->input('source');
+
+        // Get the URL of the news
+        $news->url = $request->input('url');
+
+        // Get the URL image of the news
+        $news->url_image = $request->input('url_image');
+
+        // Get the description of the news
+        $news->description = $request->input('description');
+
+        // Get the content of the news
+        $news->content = $request->input('content');
+
+        // Get the publish date of the news
+        $news->published_at = $request->input('date');
+
+        // Save the news into database
+        $news->save();
+        $news = News::all();
+
+        return back()->with('message', 'Se ha agregado la noticia correctamente!');
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Responsecontent
      */
-    public function show($id)
+    public function show()
     {
-        //
+            //
     }
 
     /**
