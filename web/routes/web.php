5a32378243a106b7244be3a4293e78c3ab2f7817
Bar011 <Bar011@alumnos.ucn.cl>
2021-01-09 04:42:36 -0300

    feat(routes):add routes of views

--- web/routes/web.php
+++ web/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (){
+    return view('registernews');
 });
+
+// Route to the news form
+Route::view('registernews', 'registernews');
+
+// Route of the news controller to store
+Route::post('registernews', [NewsController::class, 'store']);
+
+// Route of the news controller to show the stored news
+Route::get('viewnews', [NewsController::class, 'show']);
+
+// Route of the news controller to edit or update the stored news
+Route::get('edit/{id}', [NewsController::class, 'edit']);
+
+//Route of the news  controller to update the stored new
+Route::post('edit',[NewsController::class, 'update']);
