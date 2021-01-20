b7a3de29c86b48a82de9281b66716f6f04593ea4
Bar011 <Bar011@alumnos.ucn.cl>
2021-01-20 02:54:00 -0300

    style(registerview):rework of view

--- web/resources/views/registernews.blade.php
+++ web/resources/views/registernews.blade.php
@@ -2,11 +2,23 @@
     <!doctype html>
 <html>
 <!-- Image and text navbar-->
-<nav class="navbar navbar-light" style="background-color: #e3f2fd;"><!-- prueba de color-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-    <span class="navbar-brand mb-0 h1">
-            Universidad Católica del Norte
-        </span>
+        <a class="navbar-brand" href="#">Laravel News</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://127.0.0.1:8000/viewnews">Listado de noticias</a>
+                </li>
+            </ul>
+           <!-- <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>-->
+        </div>
     </div>
 </nav>
 
@@ -124,7 +136,7 @@
             <!-- date management -->
             <?php
             date_default_timezone_set('America/Santiago');
-            $date_actual = date("d-m-Y");
+            $date_actual = date('c')
             ?>
             <div class="col-sm-6">
                 <label for="date" class="col-sm-8 col-form-label">Fecha de ingreso</label>
@@ -165,7 +177,7 @@
         <!-- Grid container -->
 
         <!-- Copyright -->
-        <div class="text-center p-4" style="background-color: #e3f2fd;"">
+        <div class="text-center p-4" style="background-color: #FAFAFA;"">
             © 2021 Copyright:
             <a class="text-dark" href="https://mdbootstrap.com/">UCN - Desarrollo de Soluciones moviles</a>
         </div>
